1b3ac096d15b80d5e15ba3f7de0adbcb805ff59a
wangqing3990 <wangqing3990@126.com>
2024-01-24 01:08:53 +0800

    添加修改IP地址功能

--- 2延线BOM运行监测系统/ChangeHostWindow.xaml.cs
+++ 2延线BOM运行监测系统/ChangeHostWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Windows;
@@ -21,10 +23,20 @@ namespace _2延线BOM运行监测系统
     /// </summary>
     public partial class ChangeHostWindow : Window
     {
+        ShowLog sl = Monitor.sl;
         public ChangeHostWindow()
         {
             InitializeComponent();
-            DataObject.AddPastingHandler(part1, TextBox_Pasting);
+            uc_ip.TbxIP1.TextChanged += (s, e) => { uc_gateway.TbxIP1.Text = uc_ip.TbxIP1.Text.ToString(); };
+            uc_ip.TbxIP2.TextChanged += (s, e) => { uc_gateway.TbxIP2.Text = uc_ip.TbxIP2.Text.ToString(); };
+            uc_ip.TbxIP3.TextChanged += (s, e) => { uc_gateway.TbxIP3.Text = uc_ip.TbxIP3.Text.ToString(); };
+            uc_ip.TbxIP4.TextChanged += (s, e) => { uc_gateway.TbxIP4.Text = "254"; };
+        }
+
+        private void ChangeHostWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            uc_subnetmask.TbxIP1.Text = uc_subnetmask.TbxIP2.Text = uc_subnetmask.TbxIP3.Text = "255";
+            uc_subnetmask.TbxIP4.Text = "0";
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -35,6 +47,19 @@ namespace _2延线BOM运行监测系统
             {
                 if (!string.IsNullOrEmpty(tbHostname.Text))
                 {
+                    if (!string.IsNullOrEmpty(uc_ip.TbxIP1.Text) && !string.IsNullOrEmpty(uc_ip.TbxIP2.Text) && !string.IsNullOrEmpty(uc_ip.TbxIP3.Text) && !string.IsNullOrEmpty(uc_ip.TbxIP4.Text))
+                    {
+                        //MessageBox.Show("请输入完整的IP地址");
+                        try
+                        {
+                            changeIP();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"修改IP地址出错：{ex.Message}");
+                        }
+                    }
+
                     try
                     {
                         changeHostname();
@@ -59,7 +84,7 @@ namespace _2延线BOM运行监测系统
 
         private void changeHostname()
         {
-            ShowLog sl = Monitor.sl;
+
             RegistryKey key1 = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName", true);
             key1.SetValue("ComputerName", tbHostname.Text, RegistryValueKind.String);
             key1.Close();
@@ -73,118 +98,42 @@ namespace _2延线BOM运行监测系统
             sl.showLog($"计算机名已被修改为：{tbHostname.Text}");
         }
 
+        private void changeIP()
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            NetworkInterface networkInterface = interfaces[0];
+            string interfaceName = networkInterface.Name;
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = "cmd.exe";
+            ps.Arguments =
+            /*$"/c netsh interface ip set address \"{interfaceName}\" static {Convert.ToInt32(uc_ip.TbxIP1.Text)}.{Convert.ToInt32(uc_ip.TbxIP2.Text)}.{Convert.ToInt32(uc_ip.TbxIP3.Text)}.{Convert.ToInt32(uc_ip.TbxIP4.Text)} " +
+            $"{Convert.ToInt32(uc_subnetmask.TbxIP1.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP2.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP3.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP4.Text)} " +
+            $"{Convert.ToInt32(uc_gateway.TbxIP1.Text)}.{Convert.ToInt32(uc_gateway.TbxIP2.Text)}.{Convert.ToInt32(uc_gateway.TbxIP3.Text)}.{Convert.ToInt32(uc_gateway.TbxIP4.Text)} 1";*/
+            $"/c netsh interface ip set address \"{interfaceName}\" static {uc_ip.TbxIP1.Text}.{uc_ip.TbxIP2.Text}.{uc_ip.TbxIP3.Text}.{uc_ip.TbxIP4.Text} " +
+                $"{uc_subnetmask.TbxIP1.Text}.{uc_subnetmask.TbxIP2.Text}.{uc_subnetmask.TbxIP3.Text}.{uc_subnetmask.TbxIP4.Text} " +
+                $"{uc_gateway.TbxIP1.Text}.{uc_gateway.TbxIP2.Text}.{uc_gateway.TbxIP3.Text}.{uc_gateway.TbxIP4.Text} 1";
+            ps.CreateNoWindow = true;
+            ps.UseShellExecute = false;
+            ps.RedirectStandardOutput = true;
+            ps.RedirectStandardError = true;
+            using (Process p = Process.Start(ps))
+            {
+                p.WaitForExit();
+                p.Close();
+                /*sl.showLog($"IP地址已被修改为：{Convert.ToInt32(uc_ip.TbxIP1.Text)}.{Convert.ToInt32(uc_ip.TbxIP2.Text)}.{Convert.ToInt32(uc_ip.TbxIP3.Text)}.{Convert.ToInt32(uc_ip.TbxIP4.Text)}");
+                sl.showLog($"子网掩码已被修改为：{Convert.ToInt32(uc_subnetmask.TbxIP1.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP2.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP3.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP4.Text)}");
+                sl.showLog($"默认网关已被修改为：{Convert.ToInt32(uc_gateway.TbxIP1.Text)}.{Convert.ToInt32(uc_gateway.TbxIP2.Text)}.{Convert.ToInt32(uc_gateway.TbxIP3.Text)}.{Convert.ToInt32(uc_gateway.TbxIP4.Text)}");*/
+                sl.showLog($"IP地址已被修改为：{uc_ip.TbxIP1.Text}.{uc_ip.TbxIP2.Text}.{uc_ip.TbxIP3.Text}.{uc_ip.TbxIP4.Text}");
+                sl.showLog($"子网掩码已被修改为：{uc_subnetmask.TbxIP1.Text}.{uc_subnetmask.TbxIP2.Text}.{uc_subnetmask.TbxIP3.Text}.{uc_subnetmask.TbxIP4.Text}");
+                sl.showLog($"默认网关已被修改为：{uc_gateway.TbxIP1.Text}.{uc_gateway.TbxIP2.Text}.{uc_gateway.TbxIP3.Text}.{uc_gateway.TbxIP4.Text}");
+            }
+        }
+
         private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
 
-        private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            var vm = this.DataContext as IpAddressViewModel;
-            if (e.DataObject.GetDataPresent(typeof(String)))
-            {
-                String pastingText = (String)e.DataObject.GetData(typeof(String));
-                vm.SetAddress(pastingText);
-                part1.Focus();
-                e.CancelCommand();
-            }
 
-        }
-
-        private void Part4_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Back && part4.Text == "")
-            {
-                part3.CaretIndex = part3.Text.Length;
-                part3.Focus();
-            }
-            if (e.Key == Key.Left && part4.CaretIndex == 0)
-            {
-                part3.Focus();
-                e.Handled = true;
-            }
-
-            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control) && e.Key == Key.C)
-            {
-                if (part4.SelectionLength == 0)
-                {
-                    var vm = this.DataContext as IpAddressViewModel;
-                    Clipboard.SetText(vm.AddressText);
-                }
-            }
-        }
-
-        private void Part2_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Back && part2.Text == "")
-            {
-                part1.CaretIndex = part1.Text.Length;
-                part1.Focus();
-            }
-            if (e.Key == Key.Right && part2.CaretIndex == part2.Text.Length)
-            {
-                part3.Focus();
-                e.Handled = true;
-            }
-            if (e.Key == Key.Left && part2.CaretIndex == 0)
-            {
-                part1.Focus();
-                e.Handled = true;
-            }
-
-            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control) && e.Key == Key.C)
-            {
-                if (part2.SelectionLength == 0)
-                {
-                    var vm = this.DataContext as IpAddressViewModel;
-                    Clipboard.SetText(vm.AddressText);
-                }
-            }
-        }
-
-        private void Part3_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            if (e.Key == Key.Back && part3.Text == "")
-            {
-                part2.CaretIndex = part2.Text.Length;
-                part2.Focus();
-            }
-            if (e.Key == Key.Right && part3.CaretIndex == part3.Text.Length)
-            {
-                part4.Focus();
-                e.Handled = true;
-            }
-            if (e.Key == Key.Left && part3.CaretIndex == 0)
-            {
-                part2.Focus();
-                e.Handled = true;
-            }
-
-            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control) && e.Key == Key.C)
-            {
-                if (part3.SelectionLength == 0)
-                {
-                    var vm = this.DataContext as IpAddressViewModel;
-                    Clipboard.SetText(vm.AddressText);
-                }
-            }
-        }
-
-        private void Part1_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Right && part1.CaretIndex == part1.Text.Length)
-            {
-                part2.Focus();
-                e.Handled = true;
-            }
-            if (e.KeyboardDevice.Modifiers.HasFlag(ModifierKeys.Control) && e.Key == Key.C)
-            {
-                if (part1.SelectionLength == 0)
-                {
-                    var vm = this.DataContext as IpAddressViewModel;
-                    Clipboard.SetText(vm.AddressText);
-                }
-            }
-        }
     }
 }
---
+++ 2延线BOM运行监测系统/UserControl1.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2延线BOM运行监测系统
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void TbxIP1_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Key key = e.Key;
+            TextBox tbx = sender as TextBox;
+            if ((key >= Key.D0 && key <= Key.D9) || (key >= Key.NumPad0 && key <= Key.NumPad9))
+            {
+            }
+            else if (key == Key.Delete)
+            {
+            }
+            else if (key == Key.Enter)
+            {
+            }
+            else if (key == Key.Back)
+            {
+                // 删除光标前面的数字，如果光标前没有数字，会跳转到前面一个输入框
+                // 先Focus到了前面一个输入框，再执行的删除操作，所以会删除前面输入框中的一个数字
+                if (tbx.CaretIndex == 0)
+                {
+                    SetTbxFocus(tbx, false, false);
+                }
+            }
+            else if (key == Key.Tab)
+            {
+            }
+            else if (key == Key.Left)
+            {
+                // 光标已经在当前输入框的最左边，则跳转到前面一个输入框
+                if (tbx.CaretIndex == 0)
+                {
+                    SetTbxFocus(tbx, false, false);
+                    // 得设置true，不然光标在前面一个输入框里也会移动一次
+                    // 例如前一个输入框中的数字是123，Focus后，光标在数字3右边
+                    // 不设置true，会移动到数字2和数字3之间
+                    e.Handled = true;
+                }
+            }
+            else if (key == Key.Right)
+            {
+                // 光标已经在当前输入框的最右边，则跳转到后面一个输入框
+                if (tbx.SelectionStart == tbx.Text.Length)
+                {
+                    SetTbxFocus(tbx, true, false);
+                    // true同理
+                    e.Handled = true;
+                }
+            }
+            else
+            {
+                // 不是上述按键，就不处理
+                e.Handled = true;
+            }
+        }
+
+        private void TbxIP1_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            Key key = e.Key;
+            TextBox tbx = sender as TextBox;
+            if ((key >= Key.D0 && key <= Key.D9) || (key >= Key.NumPad0 && key <= Key.NumPad9))
+            {
+                // 当前输入框满三个数字后
+                // 跳转到后面一个输入框
+                if (tbx.Text.Length == 3)
+                {
+                    if (Int32.Parse(tbx.Text) < 0 || Int32.Parse(tbx.Text) > 255)
+                    {
+                        tbx.Text = "255";
+                        MessageBox.Show(tbx.Text + "不是有效项。请指定一个介于0和255间的值。", "错误", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                        return;
+                    }
+                    SetTbxFocus(tbx, true, true);
+                }
+            }
+            else if (key == Key.Delete)
+            {
+                // 无操作
+            }
+            else if (key == Key.Enter)
+            {
+                // 暂时不做操作
+            }
+            else if (key == Key.Back)
+            {
+            }
+            else if (key == Key.Tab)
+            {
+                // 暂时不做操作
+            }
+            else if (key == Key.Left)
+            {
+            }
+            else if (key == Key.Right)
+            {
+            }
+            else
+            {
+                // 不是上述按键，就不处理
+                e.Handled = true;
+            }
+        }
+
+        private void SetTbxFocus(TextBox curretTbx, bool isBack, bool isSelectAll)
+        {
+            // 所有的ip输入框
+            List<TextBox> TbxIPList = new List<TextBox>();
+            foreach (UIElement item in GridIPAddress.Children)
+            {
+                if (item.GetType() == typeof(TextBox))
+                {
+                    TbxIPList.Add(item as TextBox);
+                }
+            }
+            // 要聚焦的输入框
+            TextBox nextTbx = null;
+            // 往后
+            if (isBack)
+            {
+                // 当前输入框是前三个，那么就取后一个输入框
+                int index = TbxIPList.IndexOf(curretTbx);
+                if (index <= 2)
+                {
+                    nextTbx = TbxIPList[index + 1];
+                }
+            }
+            // 往前
+            else
+            {
+                // 当前输入框是后三个，那么就取前一个输入框
+                int index = TbxIPList.IndexOf(curretTbx);
+                if (index >= 1)
+                {
+                    nextTbx = TbxIPList[index - 1];
+                }
+            }
+            // 设置焦点 全选内容
+            if (nextTbx != null)
+            {
+                nextTbx.Focus();
+                if (isSelectAll)
+                {
+                    nextTbx.SelectAll();
+                }
+            }
+        }
+    }
+}
