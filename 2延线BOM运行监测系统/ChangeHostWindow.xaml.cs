efd53a44e802ab2b22794fa7addb84d4af5fdb86
wangqing <wangqing3990@126.com>
2024-01-25 09:44:09 +0800

    修复未输入完整IP地址不能修改计算机名的错误

--- 2延线BOM运行监测系统/ChangeHostWindow.xaml.cs
+++ 2延线BOM运行监测系统/ChangeHostWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading;
 using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Microsoft.Win32;
 using Button = System.Windows.Controls.Button;
 using MessageBox = System.Windows.Forms.MessageBox;
 using MessageBoxOptions = System.Windows.Forms.MessageBoxOptions;
@@ -51,14 +42,15 @@ namespace _2延线BOM运行监测系统
                 {
                     if (!string.IsNullOrEmpty(uc_ip.TbxIP1.Text) && !string.IsNullOrEmpty(uc_ip.TbxIP2.Text) && !string.IsNullOrEmpty(uc_ip.TbxIP3.Text) && !string.IsNullOrEmpty(uc_ip.TbxIP4.Text))
                     {
-                        //MessageBox.Show("请输入完整的IP地址");
                         try
                         {
                             changeIP();
+                            changeHostname();
+                            this.Close();
                         }
                         catch (Exception ex)
                         {
-                            MessageBox.Show($"修改IP地址出错：{ex.Message}");
+                            MessageBox.Show($"修改出错：{ex.Message}");
                         }
                     }
                     else
@@ -93,7 +85,6 @@ namespace _2延线BOM运行监测系统
 
         private void changeHostname()
         {
-
             RegistryKey key1 = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName", true);
             key1.SetValue("ComputerName", tbHostname.Text, RegistryValueKind.String);
             key1.Close();
@@ -114,13 +105,9 @@ namespace _2延线BOM运行监测系统
             string interfaceName = networkInterface.Name;
             ProcessStartInfo ps = new ProcessStartInfo();
             ps.FileName = "cmd.exe";
-            ps.Arguments =
-            /*$"/c netsh interface ip set address \"{interfaceName}\" static {Convert.ToInt32(uc_ip.TbxIP1.Text)}.{Convert.ToInt32(uc_ip.TbxIP2.Text)}.{Convert.ToInt32(uc_ip.TbxIP3.Text)}.{Convert.ToInt32(uc_ip.TbxIP4.Text)} " +
-            $"{Convert.ToInt32(uc_subnetmask.TbxIP1.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP2.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP3.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP4.Text)} " +
-            $"{Convert.ToInt32(uc_gateway.TbxIP1.Text)}.{Convert.ToInt32(uc_gateway.TbxIP2.Text)}.{Convert.ToInt32(uc_gateway.TbxIP3.Text)}.{Convert.ToInt32(uc_gateway.TbxIP4.Text)} 1";*/
-            $"/c netsh interface ip set address \"{interfaceName}\" static {uc_ip.TbxIP1.Text}.{uc_ip.TbxIP2.Text}.{uc_ip.TbxIP3.Text}.{uc_ip.TbxIP4.Text} " +
-                $"{uc_subnetmask.TbxIP1.Text}.{uc_subnetmask.TbxIP2.Text}.{uc_subnetmask.TbxIP3.Text}.{uc_subnetmask.TbxIP4.Text} " +
-                $"{uc_gateway.TbxIP1.Text}.{uc_gateway.TbxIP2.Text}.{uc_gateway.TbxIP3.Text}.{uc_gateway.TbxIP4.Text} 1";
+            ps.Arguments = $"/c netsh interface ip set address \"{interfaceName}\" static {uc_ip.TbxIP1.Text}.{uc_ip.TbxIP2.Text}.{uc_ip.TbxIP3.Text}.{uc_ip.TbxIP4.Text} " +
+                           $"{uc_subnetmask.TbxIP1.Text}.{uc_subnetmask.TbxIP2.Text}.{uc_subnetmask.TbxIP3.Text}.{uc_subnetmask.TbxIP4.Text} " +
+                           $"{uc_gateway.TbxIP1.Text}.{uc_gateway.TbxIP2.Text}.{uc_gateway.TbxIP3.Text}.{uc_gateway.TbxIP4.Text} 1";
             ps.CreateNoWindow = true;
             ps.UseShellExecute = false;
             ps.RedirectStandardOutput = true;
@@ -129,9 +116,6 @@ namespace _2延线BOM运行监测系统
             {
                 p.WaitForExit();
                 p.Close();
-                /*sl.showLog($"IP地址已被修改为：{Convert.ToInt32(uc_ip.TbxIP1.Text)}.{Convert.ToInt32(uc_ip.TbxIP2.Text)}.{Convert.ToInt32(uc_ip.TbxIP3.Text)}.{Convert.ToInt32(uc_ip.TbxIP4.Text)}");
-                sl.showLog($"子网掩码已被修改为：{Convert.ToInt32(uc_subnetmask.TbxIP1.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP2.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP3.Text)}.{Convert.ToInt32(uc_subnetmask.TbxIP4.Text)}");
-                sl.showLog($"默认网关已被修改为：{Convert.ToInt32(uc_gateway.TbxIP1.Text)}.{Convert.ToInt32(uc_gateway.TbxIP2.Text)}.{Convert.ToInt32(uc_gateway.TbxIP3.Text)}.{Convert.ToInt32(uc_gateway.TbxIP4.Text)}");*/
                 sl.showLog($"IP地址已被修改为：{uc_ip.TbxIP1.Text}.{uc_ip.TbxIP2.Text}.{uc_ip.TbxIP3.Text}.{uc_ip.TbxIP4.Text}");
                 sl.showLog($"子网掩码已被修改为：{uc_subnetmask.TbxIP1.Text}.{uc_subnetmask.TbxIP2.Text}.{uc_subnetmask.TbxIP3.Text}.{uc_subnetmask.TbxIP4.Text}");
                 sl.showLog($"默认网关已被修改为：{uc_gateway.TbxIP1.Text}.{uc_gateway.TbxIP2.Text}.{uc_gateway.TbxIP3.Text}.{uc_gateway.TbxIP4.Text}");
