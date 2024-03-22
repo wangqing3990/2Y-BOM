27a88a70f81ca77293fc039d3636dc5b0945fbc4
wangqing <wangqing3990@126.com>
2024-03-22 16:52:34 +0800

    修复与服务端不能持续连接的bug

--- 2延线BOM运行监测系统/Client.cs
+++ 2延线BOM运行监测系统/Client.cs
@@ -9,43 +9,32 @@ namespace _2延线BOM运行监测系统
     class Client
     {
         static ShowLog sl = new ShowLog();
-        private static TcpClient tcpclnt;
-        private static readonly TimeSpan reconnectInterval = TimeSpan.FromSeconds(5);
-        private static readonly TimeSpan sendInterval = TimeSpan.FromSeconds(30);
+        private static TcpClient tcpclnt = null;
+        private static readonly TimeSpan sendInterval = TimeSpan.FromSeconds(10);
         private static DateTime lastSendTime = DateTime.MinValue;
 
         public static void connectServer(CancellationTokenSource cts)
         {
             while (!cts.IsCancellationRequested)
             {
-                if (tcpclnt == null)
+                try
                 {
-                    tcpclnt = new TcpClient();
-                }
-
-                sl.showLog(tcpclnt.Connected.ToString());
-
-                if (!tcpclnt.Connected)
-                {
-                    ConnectToServer();
-                    Thread.Sleep(reconnectInterval);
-                }
-                else
-                {
-                    if ((DateTime.Now - lastSendTime) > sendInterval)
+                    if (tcpclnt == null || !tcpclnt.Connected)
                     {
-                        try
-                        {
-                            SendVersionInformation();
-                            lastSendTime = DateTime.Now;
-                        }
-                        catch (Exception ex)
-                        {
-                            sl.showLog("Error: " + ex.Message);
-                            tcpclnt.Close();
-                            tcpclnt = null;
-                        }
+                        ConnectToServer();
                     }
+
+                    sl.showLog(tcpclnt.Connected.ToString());
+
+                    if ((DateTime.Now - lastSendTime) > sendInterval && tcpclnt.Connected)
+                    {
+                        SendVersionInformation();
+                        lastSendTime = DateTime.Now;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    sl.showLog("Error1: " + ex.Message);
                 }
                 Thread.Sleep(1000);
             }
@@ -56,28 +45,50 @@ namespace _2延线BOM运行监测系统
             try
             {
                 sl.showLog("连接服务器中...");
+                tcpclnt = new TcpClient();
                 tcpclnt.Connect("172.22.50.3", 8888);
                 sl.showLog("连接服务器成功");
+                return;
             }
             catch (Exception ex)
             {
                 sl.showLog("连接服务器失败: " + ex.Message);
-                tcpclnt.Close();
-                tcpclnt = null;
+                if (tcpclnt != null)
+                {
+                    tcpclnt.Close();
+                    tcpclnt = null;
+                }
             }
         }
 
         private static void SendVersionInformation()
         {
-            string version = $"V {Assembly.GetEntryAssembly()?.GetName().Version}";
+            try
+            {
+                string version = $"V {Assembly.GetEntryAssembly()?.GetName().Version}\n";
 
-            tcpclnt.NoDelay = true;
-            NetworkStream networkStream = tcpclnt.GetStream();
-            sl.showLog("Sending version information...");
+                tcpclnt.NoDelay = true;
 
-            byte[] versionBytes = Encoding.ASCII.GetBytes(version);
-            networkStream.Write(versionBytes, 0, versionBytes.Length);
-            networkStream.Flush();
+                /*using (NetworkStream networkStream = tcpclnt.GetStream())
+                {*/
+                    NetworkStream networkStream = tcpclnt.GetStream();
+
+                    sl.showLog("Sending version information...");
+                    byte[] versionBytes = Encoding.ASCII.GetBytes(version);
+                    networkStream.Write(versionBytes, 0, versionBytes.Length);
+                    networkStream.Flush();
+                // }
+            }
+            catch (Exception ex)
+            {
+                sl.showLog("Error2: " + ex.Message);
+                if (tcpclnt != null)
+                {
+                    tcpclnt.Close();
+                    tcpclnt = null;
+                }
+            }
+            
         }
     }
 }
